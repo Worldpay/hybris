e7c129e8e1f587606fc80235344065b98a2ef5a2
Adrian Barrena <adrian.barrena@e2ycommerce.com>
2024-02-22 14:25:02 +0100

    Release 2205-4.1

--- hybris/bin/y-ext/ext-worldpay/worldpayapi/testsrc/com/worldpay/converters/populators/internal/model/BrowserPopulatorTest.java
+++ hybris/bin/y-ext/ext-worldpay/worldpayapi/testsrc/com/worldpay/converters/populators/internal/model/BrowserPopulatorTest.java
@@ -17,9 +17,15 @@ public class BrowserPopulatorTest {
 
     private static final String ACCEPTER_HEADER = "accepterHeader";
     private static final String DEVICE_OS = "deviceOS";
+    private static final String DEVICE_TYPE = "deviceType";
     private static final String HTTP_ACCEPT_LANGUAGE = "httpAcceptLanguage";
     private static final String HTTP_REFERER = "httpReferer";
     private static final String USER_AGENT_HEADER = "userAgentHeader";
+    private static final String LANGUAGE = "language";
+    private static final String TIME_ZONE = "timeZone";
+    private static final int HEIGHT = 1080;
+    private static final int WIDTH = 1020;
+    private static final int COLOUR_DEPTH = 24;
 
     @InjectMocks
     BrowserPopulator testObj;
@@ -41,9 +47,18 @@ public class BrowserPopulatorTest {
     public void populate_ShouldPopulateBrowser() {
         when(sourceMock.getAcceptHeader()).thenReturn(ACCEPTER_HEADER);
         when(sourceMock.getDeviceOS()).thenReturn(DEVICE_OS);
+        when(sourceMock.getDeviceType()).thenReturn(DEVICE_TYPE);
         when(sourceMock.getHttpAcceptLanguage()).thenReturn(HTTP_ACCEPT_LANGUAGE);
         when(sourceMock.getHttpReferer()).thenReturn(HTTP_REFERER);
         when(sourceMock.getUserAgentHeader()).thenReturn(USER_AGENT_HEADER);
+
+        when(sourceMock.getLanguage()).thenReturn(LANGUAGE);
+        when(sourceMock.getTimeZone()).thenReturn(TIME_ZONE);
+        when(sourceMock.getJavascriptEnabled()).thenReturn(Boolean.TRUE);
+        when(sourceMock.getJavaEnabled()).thenReturn(Boolean.FALSE);
+        when(sourceMock.getColorDepth()).thenReturn(COLOUR_DEPTH);
+        when(sourceMock.getScreenWidth()).thenReturn(WIDTH);
+        when(sourceMock.getScreenHeight()).thenReturn(HEIGHT);
 
         final com.worldpay.internal.model.Browser targetMock = new com.worldpay.internal.model.Browser();
         testObj.populate(sourceMock, targetMock);
@@ -53,5 +68,13 @@ public class BrowserPopulatorTest {
         assertThat(targetMock.getHttpAcceptLanguage()).isEqualTo(HTTP_ACCEPT_LANGUAGE);
         assertThat(targetMock.getHttpReferer()).isEqualTo(HTTP_REFERER);
         assertThat(targetMock.getUserAgentHeader()).isEqualTo(USER_AGENT_HEADER);
+        assertThat(targetMock.getDeviceType()).isEqualTo(DEVICE_TYPE);
+        assertThat(targetMock.getBrowserLanguage()).isEqualTo(LANGUAGE);
+        assertThat(targetMock.getTimeZone()).isEqualTo(TIME_ZONE);
+        assertThat(targetMock.getBrowserJavaEnabled()).isEqualTo(Boolean.FALSE.toString());
+        assertThat(targetMock.getBrowserJavaScriptEnabled()).isEqualTo(Boolean.TRUE.toString());
+        assertThat(targetMock.getBrowserScreenHeight()).isEqualTo(String.valueOf(HEIGHT));
+        assertThat(targetMock.getBrowserScreenWidth()).isEqualTo(String.valueOf(WIDTH));
+        assertThat(targetMock.getBrowserColourDepth()).isEqualTo(String.valueOf(COLOUR_DEPTH));
     }
 }
--- hybris/bin/y-ext/ext-worldpay/worldpayextocc/testsrc/com/worldpay/worldpayextocc/controllers/AbstractWorldpayControllerTest.java
+++ hybris/bin/y-ext/ext-worldpay/worldpayextocc/testsrc/com/worldpay/worldpayextocc/controllers/AbstractWorldpayControllerTest.java
@@ -1,11 +1,16 @@
 package com.worldpay.worldpayextocc.controllers;
 
+import com.worldpay.data.Browser;
+import com.worldpay.dto.BrowserInfoWsDTO;
 import com.worldpay.dto.order.PlaceOrderResponseWsDTO;
+import com.worldpay.facades.payment.WorldpayAdditionalInfoFacade;
+import com.worldpay.order.data.WorldpayAdditionalInfoData;
 import com.worldpay.payment.DirectResponseData;
 import com.worldpay.payment.TransactionStatus;
 import de.hybris.bootstrap.annotations.UnitTest;
 import de.hybris.platform.commercefacades.order.CheckoutFacade;
 import de.hybris.platform.commercefacades.order.data.CartData;
+import de.hybris.platform.webservicescommons.mapping.DataMapper;
 import de.hybris.platform.webservicescommons.mapping.FieldSetLevelHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +23,9 @@ import org.springframework.security.oauth2.provider.authentication.OAuth2Authent
 
 import javax.servlet.http.HttpServletRequest;
 
+import static org.apache.http.HttpHeaders.USER_AGENT;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @UnitTest
@@ -30,7 +37,14 @@ public class AbstractWorldpayControllerTest {
     private static final String RETURN_CODE = "returnCode";
     private static final String PA_REQUEST = "paRequest";
     private static final String JAVAX_SERVLET_REQUEST_SSL_SESSION_ID = "javax.servlet.request.ssl_session_id";
-    public static final String TOKEN_VALUE = "1111";
+    private static final String TOKEN_VALUE = "1111";
+    private static final String TIME_ZONE = "timeZone";
+    private static final String LANGUAGE = "language";
+    private static final int SCREEN_HEIGHT = 1080;
+    private static final int SCREEN_WIDTH = 1200;
+    private static final int COLOR_DEPTH = 24;
+    private static final String CVC = "cvc";
+    private static final String USER_AGENT_HEADER = "userAgentHeader";
 
     @InjectMocks
     private AbstractWorldpayController testObj;
@@ -41,11 +55,21 @@ public class AbstractWorldpayControllerTest {
     private OAuth2AuthenticationDetails oAuth2AuthenticationDetailsMock;
     @Mock
     private Authentication authenticationMock;
+    @Mock
+    private WorldpayAdditionalInfoFacade worldpayAdditionalInfoFacadeMock;
+    @Mock
+    private DataMapper dataMapperMock;
 
     @Mock
     private HttpServletRequest requestMock;
     @Mock
     private CartData cartDataMock;
+    @Mock
+    private BrowserInfoWsDTO browserInfoWsDTOMock;
+    @Mock
+    private WorldpayAdditionalInfoData worldpayAdditionalInfoDataMock;
+    @Mock
+    private Browser broserMock;
 
     @Test
     public void shouldReturnGetSessionIdFromRequestWhenAuthenticationIsNull() {
@@ -93,6 +117,31 @@ public class AbstractWorldpayControllerTest {
 
         assertEquals(TransactionStatus.AUTHENTICATION_REQUIRED, result.getTransactionStatus());
         assertEquals(RETURN_CODE, result.getReturnCode());
+    }
+
+    @Test
+    public void createWorldpayAdditionalInfo_shouldReturnWorldpayAdditionalInfoData() {
+        when(worldpayAdditionalInfoFacadeMock.createWorldpayAdditionalInfoData(requestMock)).thenReturn(worldpayAdditionalInfoDataMock);
+        when(browserInfoWsDTOMock.getJavaEnabled()).thenReturn(Boolean.TRUE);
+        when(browserInfoWsDTOMock.getJavascriptEnabled()).thenReturn(Boolean.TRUE);
+        when(browserInfoWsDTOMock.getScreenHeight()).thenReturn(SCREEN_HEIGHT);
+        when(browserInfoWsDTOMock.getScreenWidth()).thenReturn(SCREEN_WIDTH);
+        when(browserInfoWsDTOMock.getColorDepth()).thenReturn(COLOR_DEPTH);
+        when(browserInfoWsDTOMock.getTimeZone()).thenReturn(TIME_ZONE);
+        when(browserInfoWsDTOMock.getLanguage()).thenReturn(LANGUAGE);
+        when(requestMock.getAttribute(JAVAX_SERVLET_REQUEST_SSL_SESSION_ID)).thenReturn(SESSION_ID);
+
+        final WorldpayAdditionalInfoData result = testObj.createWorldpayAdditionalInfo(requestMock, CVC, browserInfoWsDTOMock);
+
+        verify(result).setSessionId(SESSION_ID);
+        verify(result).setSecurityCode(CVC);
+        verify(result).setJavaEnabled(Boolean.TRUE);
+        verify(result).setJavascriptEnabled(Boolean.TRUE);
+        verify(result).setTimeZone(TIME_ZONE);
+        verify(result).setScreenHeight(SCREEN_HEIGHT);
+        verify(result).setScreenWidth(SCREEN_WIDTH);
+        verify(result).setLanguage(LANGUAGE);
+        verify(result).setColorDepth(COLOR_DEPTH);
     }
 
 }
